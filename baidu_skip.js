003bac0d5d7e1d1351f2c3cd6a95934e5bf20261
Eiddoel <eiddoel@163.com>
2021-09-09 12:49:15 +0800

    update baidu_skip.js

--- baidu_skip.js
+++ baidu_skip.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         百度优化+自动展开
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
+// @version      0.0.2
 // @description  优化=去广告+去重定向
 // @author       eiddoel
 // @match        http*://*.baidu.com/*
