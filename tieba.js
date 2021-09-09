5205cafd7ccaef467cd3645d709e04ebfa219a67
Eiddoel <eiddoel@163.com>
2021-09-10 01:13:52 +0800

    update tieba.js

--- tieba.js
+++ tieba.js
@@ -1,11 +1,11 @@
 /*
- * @name: 贴吧解除限制
- * @Author: Track13
- * @version: 2.4.0
- * @description: 解除不能看帖的限制，去广告
- * @include: tieba.baidu.com
- * @createTime: 2019-10-18
- * @updateTime: 2020-12-24
+ * @name 贴吧解除限制
+ * @Author Track13
+ * @version 2.4.0
+ * @description 解除不能看帖的限制，去广告
+ * @include tieba.baidu.com
+ * @createTime 2019-10-18
+ * @updateTime 2020-12-24
  */
 (function () {
   var whiteList = ['tieba.baidu.com', 'm.tieba.com', 'jump2.bdimg.com', 'tiebac.baidu.com', 'c.tieba.baidu.com'];
@@ -21,16 +21,16 @@
   let reg = /\/p\/.*/gi;
   const pathname = window.location.pathname;
   if (reg.test(pathname)) {
-    const tid = pathname.replace(/[^0-9]/ig,"");
+    const tid = pathname.replace(/[^0-9]/ig, "");
     window.location.replace(`https://byokpg.smartapps.cn/pages/pb/pb?tid=${tid}`);
     return
   };
   function QueryString(item) {
     var sValue = location.search.match(new RegExp("[\?\&]" + item + "=([^\&]*)(\&?)", "i"));
-    return sValue ? sValue[1] : sValue;
+    return sValue ? sValue[1]: sValue;
   };
-  if(pathname === '/f') {
-    if (QueryString('kz')){
+  if (pathname === '/f') {
+    if (QueryString('kz')) {
       window.location.replace(`https://byokpg.smartapps.cn/pages/pb/pb?tid=${QueryString('kz')}`);
     } else {
       const tname = QueryString('kw');
